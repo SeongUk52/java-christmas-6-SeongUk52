1819a94d8b449881676bd1c35be25d855a017d0e
SeongUk52 <rkfka7849@gmail.com>
2023-11-13 00:36:10 +0900

    test(DateFactory): 1 이상 31 이하의 숫자가 아닐 경우 예외처리하는지 테스트

---
+++ src/test/java/christmas/utils/DateFactoryTest.java
@@ -0,0 +1,17 @@
+package christmas.utils;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class DateFactoryTest {
+    @DisplayName("범위를 벗어난 입력이 주어지면 예외처리")
+    @ValueSource(strings = {"99", "0", "-12"})
+    @ParameterizedTest
+    void createDateFromOutRange(Integer input) {
+        assertThatThrownBy(() -> DateFactory.createDateFrom(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
