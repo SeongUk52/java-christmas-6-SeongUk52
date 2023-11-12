2573746231047a8f92d252e4d92fe58e34d55169
SeongUk52 <rkfka7849@gmail.com>
2023-11-13 00:49:07 +0900

    feat(DateFactory): 범위를 벗어난 입력에 대한 예외처리

--- src/main/java/christmas/constants/SystemConstant.java
+++ src/main/java/christmas/constants/SystemConstant.java
@@ -2,7 +2,9 @@ package christmas.constants;
 
 public enum SystemConstant {
     EVENT_YEAR(2023),
-    EVENT_MONTH(12);
+    EVENT_MONTH(12),
+    FIRST_OF_DAYS(1),
+    END_OF_DAYS(31);
 
     private final int value;
 
--- src/main/java/christmas/utils/DateFactory.java
+++ src/main/java/christmas/utils/DateFactory.java
@@ -1,7 +1,9 @@
 package christmas.utils;
 
+import static christmas.constants.SystemConstant.END_OF_DAYS;
 import static christmas.constants.SystemConstant.EVENT_MONTH;
 import static christmas.constants.SystemConstant.EVENT_YEAR;
+import static christmas.constants.SystemConstant.FIRST_OF_DAYS;
 
 import java.util.Date;
 
@@ -9,11 +11,22 @@ public class DateFactory {
     private DateFactory() {
     }
 
-    public static Date createDateFrom(String date) {
-        return new Date(EVENT_YEAR.getValue(), EVENT_MONTH.getValue(), Integer.parseInt(date));
+    private static Date createDateFrom(int date) {
+        validateRange(date);
+        return new Date(EVENT_YEAR.getValue(), EVENT_MONTH.getValue(), date);
     }
 
-    private void validate(String date) {
+    public static Date createDateFrom(String date) {
+        return createDateFrom(Integer.parseInt(date));
+    }
 
+    private static void validateRange(int date) {
+        if (!isInRange(date)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isInRange(int date) {
+        return date > FIRST_OF_DAYS.getValue() && date < END_OF_DAYS.getValue();
     }
 }
