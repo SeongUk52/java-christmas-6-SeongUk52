acc9a43fe5815cff67ef1141ac9c6b34fa9af3dd
SeongUk52 <rkfka7849@gmail.com>
2023-11-14 02:42:01 +0900

    test(BenefitsManagerTest): 총 혜택 금액 계산 기능 테스트

--- src/test/java/christmas/domain/BenefitsManagerTest.java
+++ src/test/java/christmas/domain/BenefitsManagerTest.java
@@ -1,5 +1,9 @@
 package christmas.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +12,10 @@ public class BenefitsManagerTest {
     @DisplayName("총 할인률을 계산하는 기능")
     @Test
     void calculateTotalBenefits() {
-        //TODO 2023-11-13 19:37 테스트 코드 작성
+        assertThat(BenefitsManager.of(LocalDate.ofEpochDay(23), 70_000 + 60_000 + 15_000, List
+                .of(Dish.from("해산물파스타-2"),
+                        Dish.from("레드와인-1"),
+                        Dish.from("초코케이크-1"))).calculateTotalBenefits())
+                .isEqualTo(3_300 + 2_023 + 1_000 + 25_000);
     }
 }
