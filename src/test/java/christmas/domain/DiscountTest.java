a765329a6904fad37fe13d8c4f285f2f51313dd7
SeongUk52 <rkfka7849@gmail.com>
2023-11-14 21:43:14 +0900

    test(Discount): 할인 가격이 0원일 때 toMessage()가 null값을 반환하는지 테스트

--- src/test/java/christmas/domain/DiscountTest.java
+++ src/test/java/christmas/domain/DiscountTest.java
@@ -1,8 +1,8 @@
 package christmas.domain;
 
+import static christmas.data.DiscountData.SPECIAL_DISCOUNT;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import christmas.data.DiscountData;
 import christmas.utils.LocalDateFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,9 +10,16 @@ import org.junit.jupiter.api.Test;
 public class DiscountTest {
     @DisplayName("문자열 변환 기능 테스트")
     @Test
-    void DiscountToString() {
-        assertThat(Discount.of(DiscountData.SPECIAL_DISCOUNT, LocalDateFactory.createLocalDateFrom("3"))
+    void discountToString() {
+        assertThat(Discount.of(SPECIAL_DISCOUNT, LocalDateFactory.createLocalDateFrom("3"))
                 .toString())
                 .isEqualTo("특별 할인");
+    }
+
+    @DisplayName("할인가격이 0인 할인은 null을 반환")
+    @Test
+    void returnNull() {
+        assertThat(Discount.of(SPECIAL_DISCOUNT, LocalDateFactory.createLocalDateFrom("2"))
+                .toMessage()).isEqualTo(null);
     }
 }
