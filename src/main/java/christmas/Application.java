9e2b5abcfbff84639c4e8c99578db258176d63af
SeongUk52 <rkfka7849@gmail.com>
2023-11-15 21:15:42 +0900

    refactor(Application): 타입을 명시하는 대신 var 사용
    생성자에서 타입을 유추할 수 있기 때문에 var를 사용해도 괜찮다고 판단함

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -6,7 +6,7 @@ import christmas.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        EventPlannerController eventPlannerController = new EventPlannerController(new InputView(), new OutputView());
+        var eventPlannerController = new EventPlannerController(new InputView(), new OutputView());
         eventPlannerController.play();
     }
 }
